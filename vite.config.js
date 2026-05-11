7f344393e05ffb1123cf0f2cb5cee669fbf744b8
GeraldusCiterTeja <duateja@gmail.com>
2026-05-11 19:10:01 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'icons/icon.png',
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable',
           },
           {
-            src: 'icons/icon.png',
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
             type: 'image/png',
           },
         ],
